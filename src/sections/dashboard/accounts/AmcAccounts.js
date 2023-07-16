e952ba00437166bae7f1aa5759b82478318fce4f
devance <dj788@snu.edu.in>
2023-07-16 17:08:25 +0530

    added pdf for accounting

--- src/sections/dashboard/accounts/AmcAccounts.js
+++ src/sections/dashboard/accounts/AmcAccounts.js
@@ -1,13 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { Typography, Button } from "@mui/material";
 import { Scrollbar } from "src/components/scrollbar";
 import { Table } from "antd";
 import { Box } from "@mui/system";
-
 import { apiUrl } from "src/config";
-
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "../pdfAssets/vfs_fonts";
+import { LogoContext } from 'src/utils/logoContext'
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+pdfMake.fonts = {
+  Helvetica: {
+    normal: 'Helvetica.ttf',
+    bold: 'Helvetica-Bold.ttf',
+  }
+};
 const AmcAccounts = ({ year, category }) => {
+  const { logo } = useContext(LogoContext);
   const [userData, setUserData] = useState({});
 
   useEffect(() => {
@@ -111,6 +120,175 @@ const AmcAccounts = ({ year, category }) => {
         },
       ];
 
+      const handlePDF = () => {
+        try {
+          console.log(dataWithKeys)
+          const rowData = dataWithKeys.map((item, index) => {
+            return [
+              { text: index + 1, style: "tableContent" },
+              { text: formatDate(item.createdDate), style: "tableContent" },
+              { text: item.id, style: "tableContent" },
+              { text: item.companyName, style: "tableContent" },
+              { text: item.gstn, style: "tableContent" },
+              { text: item.totalcost, style: "tableContent" },
+              { text: item.totaligst, style: "tableContent" },
+              { text: item.totalcgst, style: "tableContent" },
+              { text: item.totalsgst, style: "tableContent" },
+              { text: item.totalamount, style: "tableContent" },
+              { text: item.totalamount - item.paidamount, style: "tableContent" },
+            ]
+          });
+          const docDefinition = {
+            pageOrientation: "landscape",
+            defaultStyle: {
+              font: "Helvetica",
+            },
+            content: [
+              {
+                table: {
+                  widths: "*",
+                  body: [
+                    [
+                      {
+                        columns: [
+                          {
+                            image: `data:${logo.fileType};base64, ${logo.file}`,
+                            // image: pdfLogo,
+                            width: 100,
+                            alignment: "left",
+                          },
+                          {
+                            text: `AMC Details of ${month} ${year}`,
+                            style: "header",
+                            alignment: "center",
+                          },
+
+                        ],
+                        border: [true, true, true, false],
+                        margin: [0, 10, 0, 20],
+                      },
+                    ],
+                  ],
+                },
+              },
+              {
+                style: "table",
+                table: {
+                  widths: [
+                    "auto",
+                    "auto",
+                    "auto",
+                    "*",
+                    "auto",
+                    "auto",
+                    "auto",
+                    "auto",
+                    "auto",
+                    "auto",
+                    "auto",
+                  ],
+                  headerRows: 1,
+                  // heights: [
+                  //   "auto",
+                  //   ...(rowData.length > 0
+                  //     ? Array(rowData.length - 1)
+                  //       .fill(0)
+                  //       .concat([100 - (rowData.length - 1) * 20])
+                  //     : [120]),
+                  // ],
+                  body: [
+                    [
+                      { text: "S.No.", style: "tableLabel" },
+                      { text: "Order Date", style: "tableLabel" },
+                      { text: "Invoice No.", style: "tableLabel" },
+                      { text: "Customer Name", style: "tableLabel" },
+                      { text: "GSTIN", style: "tableLabel" },
+                      { text: "Taxable Supply", style: "tableLabel" },
+                      { text: "Total IGST", style: "tableLabel" },
+                      { text: "Total CGST", style: "tableLabel" },
+                      { text: "Total SGST", style: "tableLabel" },
+                      { text: "Total Amount", style: "tableLabel" },
+                      { text: "Pending Amount", style: "tableLabel" },
+                    ],
+                    ...rowData,
+                    [
+                      { text: "Total", style: "tableLabel" },
+                      { text: "", style: "tableLabel" },
+                      { text: "", style: "tableLabel" },
+                      { text: "", style: "tableLabel" },
+                      { text: "", style: "tableLabel" },
+                      { text: dataWithKeys?.reduce((total, item) => total + item.totalcost, 0).toFixed(2), style: "tableLabel" },
+                      { text: dataWithKeys?.reduce((total, item) => total + item.totaligst, 0).toFixed(2), style: "tableLabel" },
+                      { text: dataWithKeys?.reduce((total, item) => total + item.totalcgst, 0).toFixed(2), style: "tableLabel" },
+                      { text: dataWithKeys?.reduce((total, item) => total + item.totalsgst, 0).toFixed(2), style: "tableLabel" },
+                      { text: dataWithKeys?.reduce((total, item) => total + item.totalamount, 0).toFixed(2), style: "tableLabel" },
+                      { text: dataWithKeys?.reduce((total, item) => total + item.pendingAmount, 0).toFixed(2), style: "tableLabel" },
+                    ]
+                  ],
+                },
+                layout: {
+                  hLineWidth: function (i, node) {
+                    return i === 0 || i === node.table.body.length ? 1 : 1;
+                  },
+                  vLineWidth: function (i, node) {
+                    return i === 0 || i === node.table.widths.length ? 1 : 1;
+                  },
+                  hLineColor: function (i, node) {
+                    return i === 0 || i === node.table.body.length
+                      ? "black"
+                      : "gray";
+                  },
+                  vLineColor: function (i, node) {
+                    return i === 0 || i === node.table.widths.length
+                      ? "black"
+                      : "gray";
+                  },
+                  paddingTop: function () {
+                    return 5;
+                  },
+                  paddingBottom: function () {
+                    return 5;
+                  },
+                },
+              },
+            ],
+            styles: {
+              header: {
+                fontSize: 15,
+                bold: true,
+                margin: [0, 0, 0, 5],
+              },
+              subheader: {
+                fontSize: 12,
+                marginBottom: 5,
+              },
+              tableLabel: {
+                bold: true,
+                fontSize: 10,
+                // border: [false, false, false, true],
+              },
+              tableContent: {
+                fontSize: 10,
+              },
+              font10: {
+                fontSize: 10,
+              },
+              tableCell: {
+                fontSize: 8,
+              },
+              tableHeader: {
+                fillColor: "#eeeeee",
+                bold: true,
+              },
+            },
+          }
+          pdfMake.createPdf(docDefinition).open();
+
+        } catch (error) {
+          console.error(error)
+        }
+      }
+
       return (
         <Box key={month} sx={{ mt: 3 }}>
           <Box
@@ -121,7 +299,7 @@ const AmcAccounts = ({ year, category }) => {
               marginBottom: 1,
             }}
           >
-            <Button color="primary" variant="contained" align="right">
+            <Button color="primary" variant="contained" align="right" onClick={() => handlePDF()}>
               Generate PDF
             </Button>
           </Box>
